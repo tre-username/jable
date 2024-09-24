1001e32e729f8a529971a7bb03abf46bd930352e
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-23 21:19:30 -0700

    added dark mode, installed prisma

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton
-        afterSignOutUrl="/"
-      />
+    <div className="flex flex-col">
+      {/* User button is rendered first */}
+      <UserButton afterSignOutUrl="/" />
+      {/* ModeToggle will appear below the UserButton */}
+      <ModeToggle />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Open_Sans } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -20,11 +21,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={font.className}>
+          className={cn(
+            font.className,
+            "bg-white dark:bg-[#313338]"
+            )}>
             <ThemeProvider
               attribute="class"
               defaultTheme="dark"
-              forcedTheme="dark"
               enableSystem={false}
               storageKey="jable-theme"
             >
