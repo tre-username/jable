b1389f10269914c74730a8d60ed13d62bffd29a6
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-24 16:06:01 -0700

    added some components

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,25 +1,35 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
-    const profile = await initialProfile();
+  const profile = await initialProfile();
 
-    const server = await db.server.findFirst({
-        where: {
-            members: {
-                some: {
-                    profileId: profile.id
-                }
-            }
-        }
-    });
+  if (profile instanceof NextResponse) {
+    return profile; 
+  }
 
-    if(server) {
-        return redirect(`/servers/${server.id}`);
-    }
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
-    return <div>Create a Server</div>
+  if (!server) {
+    console.log("No server found for this profile");
+  }
+
+  if(server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />
 }
 
 export default SetupPage;
