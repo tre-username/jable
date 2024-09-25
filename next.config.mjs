2adde56ec14cec6bf19ea28dc6ce19ad1fd4a189
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-24 17:55:33 -0700

    added support for netlify hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Enables React Strict Mode for better debugging
+    images: {
+      // Add any domains you need to load images from
+      domains: ['https://benevolent-kelpie-864e24.netlify.app'], // Update this with actual domains
+    },
+    trailingSlash: true, // Optional: Adds a trailing slash to all pages
+  };
+  
+  export default nextConfig;
+  
