caf6b022d86304b04860a8c3b4a90d68ef1527b6
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-24 17:17:26 -0700

    fixed eslint error to meet standards

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,24 @@
-/* eslint-disable */
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//     var prisma: PrismaClient | undefined;
+// };
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if(process.env.NODE_ENV !== "production") globalThis.prisma = db
 
 import { PrismaClient } from "@prisma/client";
 
+// Declare a global variable for the Prisma client to avoid multiple instances in development
 declare global {
-    var prisma: PrismaClient | undefined;
-};
+  var prisma: PrismaClient | undefined;
+}
 
-export const db = globalThis.prisma || new PrismaClient();
+// Create a new instance of PrismaClient or use the existing global instance
+export const db = global.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = db
+// Assign the global variable in non-production environments
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = db;
+}
