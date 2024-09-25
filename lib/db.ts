5c987ac1c1a37859fd2ad9207d8045d20d5c5e7a
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-24 17:31:05 -0700

    disabled the "no-var" rule for eslintrc.json

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,11 @@
-// import { PrismaClient } from "@prisma/client";
-
-// declare global {
-//     var prisma: PrismaClient | undefined;
-// };
-
-// export const db = globalThis.prisma || new PrismaClient();
-
-// if(process.env.NODE_ENV !== "production") globalThis.prisma = db
-
 import { PrismaClient } from "@prisma/client";
 
-// Declare a global variable for the Prisma client to avoid multiple instances in development
+// Disable the ESLint rule for the following line
+/* eslint-disable-next-line no-var */
 declare global {
-  var prisma: PrismaClient | undefined;
+    var prisma: PrismaClient | undefined;
 }
 
-// Create a new instance of PrismaClient or use the existing global instance
-export const db = global.prisma || new PrismaClient();
+export const db = globalThis.prisma || new PrismaClient();
 
-// Assign the global variable in non-production environments
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = db;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
