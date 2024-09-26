84716af16a650aa0e11b03722785f7d541c48cf8
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-25 19:39:15 -0700

    yuh

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { UploadDropzone } from "@/lib/uploadthing";
 
 import { X } from "lucide-react";
@@ -27,6 +29,13 @@ export const FileUpload = ({
                 alt="Upload"
                 className="rounded-full"
               />
+              <button
+                onClick={() => onChange("")}
+                className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                type="button"
+              >
+                <X className="h-4 w-4" />
+              </button>
             </div>
         )
     }
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: [
-        "uploadthing.com"
-      ]
-    },
-    trailingSlash: true,
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  trailingSlash: true,
+};
+
+export default nextConfig;
