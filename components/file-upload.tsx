fd65845be48a78529ece461382856f753ca66694
SirChub <150428366+SirChub@users.noreply.github.com>
2024-09-27 08:53:52 -0700

    uploadthing works but doesnt display on localhost

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { UploadDropzone } from "@/lib/uploadthing";
-
 import { X } from "lucide-react";
 import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
 
 import "@uploadthing/react/styles.css";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: "messageFile" | "serverImage";
+  endpoint: "messageFile" | "serverImage"
 }
 
 export const FileUpload = ({
@@ -18,38 +18,30 @@ export const FileUpload = ({
   value,
   endpoint
 }: FileUploadProps) => {
-    const fileType = value?.split(".").pop();
+  const fileType = value?.split(".").pop();
 
-    if(value && fileType !== "pdf") {
-        return (
-            <div className="relative h-20 w-20">
-              <Image 
-                fill
-                src={value}
-                alt="Upload"
-                className="rounded-full"
-              />
-              <button
-                onClick={() => onChange("")}
-                className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
-                type="button"
-              >
-                <X className="h-4 w-4" />
-              </button>
-            </div>
-        )
-    }
+  if(value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image 
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
+        />
+      </div>
+    )
+  }
 
   return (
-    <UploadDropzone 
+    <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-        console.log(res);
-        onChange(res?.[0].url); 
+        onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
         console.log(error);
       }}
     />
-  );
-};
+  )
+}
