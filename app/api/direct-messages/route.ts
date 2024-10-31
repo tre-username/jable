9cfe63d8f3d1096a99f09849108822358eb4d1ed
SirChub <150428366+SirChub@users.noreply.github.com>
2024-10-30 19:54:00 -0700

    NOOOOOOO

--- app/api/direct-messages/route.ts
+++ app/api/direct-messages/route.ts
@@ -1,34 +1,32 @@
-import { NextResponse, NextRequest } from "next/server";
-import { Message } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { DirectMessage } from "@prisma/client";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 const MESSAGES_BATCH = 10;
 
-export const runtime = "nodejs"; // Ensure dynamic handling
-
-export async function GET(req: NextRequest) {
+export async function GET(req: Request) {
   try {
-    const profile = await currentProfile();
+    const profile = await currentProfile(); // Get current profile
     const { searchParams } = new URL(req.url);
 
     const cursor = searchParams.get("cursor");
-    const channelId = searchParams.get("channelId");
+    const conversationId = searchParams.get("conversationId");
 
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
-    if (!channelId) return new NextResponse("Channel ID Missing", { status: 400 });
+    if (!conversationId) return new NextResponse("Conversation ID Missing", { status: 400 });
 
-    let messages: Message[] = [];
+    let messages: DirectMessage[] = [];
 
     if (cursor) {
-      messages = await db.message.findMany({
+      messages = await db.directMessage.findMany({
         take: MESSAGES_BATCH,
         skip: 1,
         cursor: {
           id: cursor,
         },
         where: {
-          channelId,
+          conversationId,
         },
         include: {
           member: {
@@ -40,9 +38,9 @@ export async function GET(req: NextRequest) {
         orderBy: { createdAt: "asc" },
       });
     } else {
-      messages = await db.message.findMany({
+      messages = await db.directMessage.findMany({
         take: MESSAGES_BATCH,
-        where: { channelId },
+        where: { conversationId },
         include: {
           member: {
             include: {
@@ -61,7 +59,7 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.json({ items: messages, nextCursor });
   } catch (error) {
-    console.error("[MESSAGES_GET]", error);
+    console.error("[DIRECT_MESSAGES_GET]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
